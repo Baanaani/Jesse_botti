608c0998b6b46e484e0a4400bfc79be60a85f7fc
Jesse Niemelä <jesse.niemela@opiskelija.lhkk.fi>
2023-04-13 10:05:29 +0300

    init

---
+++ commands/queue.js
@@ -0,0 +1,40 @@
+const { useQueue } = require('discord-player')
+
+module.exports = {
+    name : 'queue',
+    description : 'Näyttää soittolistan sisällön!',
+    voiceChannel : true,
+
+    async execute(interaction) {
+        try {
+            const channel = interaction.member.voice.channel;
+            if (!channel) return interaction.reply('You are not connected to a voice channel!');
+
+            const queue = useQueue(interaction.guildId);
+
+            if (!queue) { return interaction.reply(`Ei biisejä soitossa`) }
+            const formatTracks = queue.tracks.toArray();
+
+            const chunkSize = 20;
+            //const pages = Math.ceil(formatTracks.length / chunkSize);
+
+            if (formatTracks.length === 0) {
+                return interaction.reply({ content: `Ei kappaleita jonossa`, ephemeral: true, });
+            }
+
+            if (formatTracks.length <= chunkSize) {
+                const tracks = formatTracks.map(
+                    (track, idx) => `**${idx + 1}.** ${track.title} \n`);
+                console.log(`Biisit: ${tracks}`);
+                await interaction.reply({content: `**Soittolista:** \n ${tracks}`});
+            } else {
+                const tracks = formatTracks.map(
+                    (track, idx) => `**${idx + 1}.** ${track.title}`);
+                console.log(`Biisit: ${tracks}`);
+                await interaction.reply({content: `**Soittolistan ${chunkSize} seuraavaa kappaletta:** \n ${tracks.slice(0, chunkSize).join("\n")}
+                    **Soittolistalla on vielä ${formatTracks.length-chunkSize} muuta kappaletta.**`});
+                 }
+
+        } catch (error) {
+            console.log(error) }
+    }}
